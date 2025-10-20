46bddc0d0ab8a69c6c1c91b3f56533178fd613f3
Manitusitu <paquitopan101@outlook.com>
2025-10-20 12:00:31 +0200

    tuplas

---
+++ Modulo1/tuplas.py
@@ -0,0 +1,15 @@
+"""
+Crea una tupla llamada producto que contenga la siguiente información en este orden: nombre del producto ("Laptop"), precio (1200.50) y disponibilidad (True).
+
+Luego, realiza las siguientes operaciones:
+
+Imprime el tipo de dato de la variable producto usando la función type()
+Accede e imprime el nombre del producto (primer elemento)
+Accede e imprime el precio (segundo elemento)
+Utiliza el desempaquetado de tuplas para asignar cada valor a tres variables llamadas nombre, precio y disponible respectivamente
+Imprime un mensaje con el siguiente formato usando las variables desempaquetadas: "El producto [nombre] cuesta [precio] euros y [está/no está] disponible" 
+(donde la disponibilidad debe mostrarse como "está disponible" o "no está disponible" según el valor booleano)
+
+"""
+
+
