9ef5f8961d6b91c4e3e8f07717bc9b9174233b4f
Manitusitu <paquitopan101@outlook.com>
2025-10-24 09:47:19 +0200

    Creado modulo2

---
+++ Modulo1/excepciones.py
@@ -0,0 +1,16 @@
+"""
+Crea una función llamada dividir_numeros que reciba como entrada dos valores proporcionados por el usuario y realice una división entre ellos. La función debe:
+
+Solicitar al usuario que introduzca dos números (usando la función input())
+Convertir las entradas a números enteros
+Realizar la división del primer número entre el segundo
+Devolver el resultado de la división
+La función debe manejar correctamente las siguientes excepciones:
+
+Si el usuario introduce algo que no se puede convertir a un entero, mostrar el mensaje "Error: Debes introducir un número válido"
+Si el usuario intenta dividir entre cero, mostrar el mensaje "Error: No es posible dividir entre cero"
+Finalmente, independientemente de si la operación tuvo éxito o no, la función debe mostrar el mensaje "Operación finalizada".
+
+"""
+
+
